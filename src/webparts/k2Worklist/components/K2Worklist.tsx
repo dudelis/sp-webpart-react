fc49ae98f582db4081250b197d6c6b5045d6f1d4
Konstantin Fukszon <fukszon.kostya@gmail.com>
2019-03-09 17:13:39 -0800

    Add list layout, populate with sample data

--- src/webparts/k2Worklist/components/K2Worklist.tsx
+++ src/webparts/k2Worklist/components/K2Worklist.tsx
@@ -2,29 +2,111 @@ import * as React from 'react';
 import styles from './K2Worklist.module.scss';
 import { IK2WorklistProps } from './IK2WorklistProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { DetailsList, DetailsListLayoutMode, Selection, IColumn } from 'office-ui-fabric-react/lib/DetailsList';
+import { Link } from 'office-ui-fabric-react/lib/Link';
+import { MarqueeSelection } from 'office-ui-fabric-react/lib/MarqueeSelection';
+import sample from '../sample/sampledata';
 
 
+export interface IK2WorklistItem {
+  workflowInstanceFolio: string;
+  status: string;
+  workflowName: string;
+  workflowDisplayName: string;
+  serialNumber: string;
+  activityName: string;
+  formURL: string;
+  viewFlowURL: string;
+}
 
-export default class K2Worklist extends React.Component<IK2WorklistProps, {}> {
+export interface IK2WorklistState {
+  items: IK2WorklistItem[];
+  selectedItems: IK2WorklistItem[];
+}
+
+export default class K2Worklist extends React.Component<IK2WorklistProps, IK2WorklistState> {
+  private _selection: Selection;
+  private _allItems: IK2WorklistItem[];
+  private _columns: IColumn[];
+
+  constructor (props: IK2WorklistProps){
+    super(props);
+
+    this._selection = new Selection({
+      onSelectionChanged: () => this.setState({ selectedItems: this._getSelectedItems() })
+    });
+    this._allItems = sample.tasks;
+
+    this._columns = [
+      { 
+        key: 'workflowInstanceFolio',
+        name: 'Folio',
+        fieldName: 'workflowInstanceFolio',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true, 
+        onRender: item => (
+          <Link key={item.serialNumber} href={item.formURL} target="_blank" >
+            {item.workflowInstanceFolio}
+          </Link>
+        )
+    },
+      {
+        key: 'activityName',
+        name: 'Activity',
+        fieldName: 'activityName',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true
+      },
+      {
+        key: 'taskStartDate',
+        name: 'Task Start Date',
+        fieldName: 'taskStartDate',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true,
+        onRender: item => (new Date(item.taskStartDate)).toUTCString()
+      },
+      {
+        key: 'workflowName',
+        name: 'Workflow',
+        fieldName: 'workflowName',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true
+      }
+    ];
+
+    this.state = {
+      items: this._allItems,
+      selectedItems: []
+    };
+  }
+
   public render(): React.ReactElement<IK2WorklistProps> {
+    const { items, selectedItems } = this.state;
+    
     return (
       <div className={ styles.ticTacToev3 }>
         <div className={ styles.container }>
-
-        {/*
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        */}
+        <MarqueeSelection selection={this._selection} data-is-scrollable="true">
+          <DetailsList
+            items={items}
+            columns={this._columns}
+            setKey="set"
+            layoutMode={DetailsListLayoutMode.fixedColumns}
+            selection={this._selection}
+            selectionPreservedOnEmptyClick={true}
+            ariaLabelForSelectionColumn="Toggle selection"
+            ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+          />
+        </MarqueeSelection>
         </div>
       </div>
     );
+  }
+  private _getSelectedItems():  any {
+    return this._selection.getSelection();
   }
 }
---
+++ src/webparts/k2Worklist/sample/sampledata.ts
@@ -0,0 +1,386 @@
+
+
+
+const sampleData = 
+{
+    "itemCount": 9,
+    "tasks": [
+      {
+        "serialNumber": "1_48",
+        "status": "Available",
+        "taskStartDate": "2018-03-26T01:30:55.7",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/Form/Editportal__denallix__com__Policy__ReviewDocumentForm/?_State=&SerialNo=1_48&ID=1",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=1",
+        "workflowID": 6069,
+        "workflowName": "Portal Policy Review\\Policy Review",
+        "workflowDisplayName": "Policy Review",
+        "workflowCategory": "Portal Policy Review",
+        "workflowInstanceID": 1,
+        "workflowInstanceFolio": "DL-000001: Test document - Copy (1).docx",
+        "activityInstanceID": 45,
+        "activityInstanceDestinationID": 48,
+        "activityName": "Secondary Review",
+        "eventName": "Secondary Review",
+        "eventDescription": "Please review the request and take the appropriate action",
+        "originator": {
+          "username": "DENALLIX\\administrator",
+          "fqn": "K2:DENALLIX\\administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Query Initial Approval",
+            "Approve Request"
+          ],
+          "systemActions": [
+            "Allocate",
+            "Redirect",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "2_34",
+        "status": "Open",
+        "taskStartDate": "2017-03-06T03:39:52.603",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/Form/Editportal__denallix__com__Policy__ReviewDocumentForm/?_State=&SerialNo=2_34&ID=1",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=2",
+        "workflowID": 1,
+        "workflowName": "Portal Policy Review\\Policy Review",
+        "workflowDisplayName": "Policy Review",
+        "workflowCategory": "Portal Policy Review",
+        "workflowInstanceID": 2,
+        "workflowInstanceFolio": "DL-000002: Test document - Copy (1).docx",
+        "activityInstanceID": 31,
+        "activityInstanceDestinationID": 34,
+        "activityName": "Review Request",
+        "eventName": "Review Request",
+        "eventDescription": "Please review the request and take the appropriate action",
+        "originator": {
+          "username": "DENALLIX\\administrator",
+          "fqn": "K2:DENALLIX\\administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Approve Request",
+            "Decline Request",
+            "Query Request"
+          ],
+          "systemActions": [
+            "Redirect",
+            "Release",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "6162_19",
+        "status": "Open",
+        "taskStartDate": "2018-04-13T06:09:48.603",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/Form/CSM.Winwire.Form.Process/?SN=6162_19",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=6162",
+        "workflowID": 7068,
+        "workflowName": "CSM.Winwire\\Winwire process",
+        "workflowDisplayName": "Winwire process",
+        "workflowCategory": "CSM.Winwire",
+        "workflowInstanceID": 6162,
+        "workflowInstanceFolio": "1002_M&S TEST direct renewal",
+        "activityInstanceID": 16,
+        "activityInstanceDestinationID": 19,
+        "activityName": "01 - Winwire processing",
+        "eventName": "Process Winwire",
+        "eventDescription": "",
+        "originator": {
+          "username": "DENALLIX\\Administrator",
+          "fqn": "K2:DENALLIX\\Administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [
+            "Complete"
+          ],
+          "batchableActions": [],
+          "systemActions": [
+            "Redirect",
+            "Release",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "8163_16",
+        "status": "Available",
+        "taskStartDate": "2018-07-12T04:21:27.143",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/Form/com.K2.App.ITAssetRequest.Form.ReviewApproval/?SN=8163_16",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=8163",
+        "workflowID": 9070,
+        "workflowName": "com.K2.App\\ITAssetRequest.Workflow.Approval",
+        "workflowDisplayName": "ITAssetRequest.Workflow.Approval",
+        "workflowCategory": "com.K2.App",
+        "workflowInstanceID": 8163,
+        "workflowInstanceFolio": "ITAR-0000001",
+        "activityInstanceID": 13,
+        "activityInstanceDestinationID": 16,
+        "activityName": "Request Approval",
+        "eventName": "Review Approval",
+        "eventDescription": "",
+        "originator": {
+          "username": "DENALLIX\\Administrator",
+          "fqn": "K2:DENALLIX\\Administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Approve Request",
+            "Query Request",
+            "Decline Request"
+          ],
+          "systemActions": [
+            "Allocate",
+            "Redirect",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "8164_16",
+        "status": "Open",
+        "taskStartDate": "2018-07-12T02:34:42.957",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/Form/com.K2.App.ExpenseClaim.Form.ReviewClaimApproval?SN=8164_16",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=8164",
+        "workflowID": 9069,
+        "workflowName": "com.K2.App\\ExpenseClaim.Workflow.Approval",
+        "workflowDisplayName": "ExpenseClaim.Workflow.Approval",
+        "workflowCategory": "com.K2.App",
+        "workflowInstanceID": 8164,
+        "workflowInstanceFolio": "EXC-0000001",
+        "activityInstanceID": 13,
+        "activityInstanceDestinationID": 16,
+        "activityName": "Primary Approval",
+        "eventName": "Primary Approval",
+        "eventDescription": "",
+        "originator": {
+          "username": "DENALLIX\\Administrator",
+          "fqn": "K2:DENALLIX\\Administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Approve Request",
+            "Decline Request",
+            "Query Request"
+          ],
+          "systemActions": [
+            "Redirect",
+            "Release",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "8165_107",
+        "status": "Available",
+        "taskStartDate": "2018-08-05T14:37:30.393",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/form/com.K2.App.CapitalExpenditure.Form.ReviewRequest?SN=8165_107",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=8165",
+        "workflowID": 9075,
+        "workflowName": "com.K2.App\\CapitalExpenditure.Workflow.Approval",
+        "workflowDisplayName": "CapitalExpenditure.Workflow.Approval",
+        "workflowCategory": "com.K2.App",
+        "workflowInstanceID": 8165,
+        "workflowInstanceFolio": "CPX-0000002",
+        "activityInstanceID": 104,
+        "activityInstanceDestinationID": 107,
+        "activityName": "Tier Review",
+        "eventName": "Tier Review",
+        "eventDescription": "",
+        "originator": {
+          "username": "DENALLIX\\Administrator",
+          "fqn": "K2:DENALLIX\\Administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Approve Request",
+            "Decline Request",
+            "Query Request"
+          ],
+          "systemActions": [
+            "Allocate",
+            "Redirect",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "9165_75",
+        "status": "Open",
+        "taskStartDate": "2018-08-06T00:26:16.683",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/form/com.K2.App.CapitalExpenditure.Form.ReviewRequest?SN=9165_75",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=9165",
+        "workflowID": 10074,
+        "workflowName": "com.K2.App\\CapitalExpenditure.Workflow.Approval",
+        "workflowDisplayName": "CapitalExpenditure.Workflow.Approval",
+        "workflowCategory": "com.K2.App",
+        "workflowInstanceID": 9165,
+        "workflowInstanceFolio": "CPX-0010002",
+        "activityInstanceID": 72,
+        "activityInstanceDestinationID": 75,
+        "activityName": "Tier Review",
+        "eventName": "Tier Review",
+        "eventDescription": "",
+        "originator": {
+          "username": "DENALLIX\\Administrator",
+          "fqn": "K2:DENALLIX\\Administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Approve Request",
+            "Decline Request",
+            "Query Request"
+          ],
+          "systemActions": [
+            "Redirect",
+            "Release",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "9166_34",
+        "status": "Available",
+        "taskStartDate": "2019-02-04T05:06:58.763",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/Form/Editportal__denallix__com__Lists__Company__AnnouncementAnnouncementForm/?_State=&SerialNo=9166_34",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=9166",
+        "workflowID": 10075,
+        "workflowName": "Portal Company Announcement\\Company Announcement",
+        "workflowDisplayName": "Company Announcement",
+        "workflowCategory": "Portal Company Announcement",
+        "workflowInstanceID": 9166,
+        "workflowInstanceFolio": "AN-009166: Test",
+        "activityInstanceID": 31,
+        "activityInstanceDestinationID": 34,
+        "activityName": "Review Request",
+        "eventName": "Review Request",
+        "eventDescription": "Please review the request and take the appropriate action",
+        "originator": {
+          "username": "denallix\\administrator",
+          "fqn": "K2:denallix\\administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Approve Request",
+            "Decline Request",
+            "Query Request"
+          ],
+          "systemActions": [
+            "Allocate",
+            "Redirect",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      },
+      {
+        "serialNumber": "9167_34",
+        "status": "Open",
+        "taskStartDate": "2019-02-04T05:07:06.137",
+        "priority": 1,
+        "formURL": "https://k2.denallix.com/Runtime/Runtime/Form/Editportal__denallix__com__Lists__Company__AnnouncementAnnouncementForm/?_State=&SerialNo=9167_34",
+        "viewFlowURL": "https://k2.denallix.com/ViewFlow/ViewFlow.aspx?ProcessID=9167",
+        "workflowID": 10076,
+        "workflowName": "Portal Company Announcement\\Company Announcement",
+        "workflowDisplayName": "Company Announcement",
+        "workflowCategory": "Portal Company Announcement",
+        "workflowInstanceID": 9167,
+        "workflowInstanceFolio": "AN-009167: Test 1",
+        "activityInstanceID": 31,
+        "activityInstanceDestinationID": 34,
+        "activityName": "Review Request",
+        "eventName": "Review Request",
+        "eventDescription": "Please review the request and take the appropriate action",
+        "originator": {
+          "username": "denallix\\administrator",
+          "fqn": "K2:denallix\\administrator",
+          "email": "administrator@denallix.com",
+          "manager": "DENALLIX\\Jonno",
+          "displayName": ""
+        },
+        "actions": {
+          "nonBatchableActions": [],
+          "batchableActions": [
+            "Approve Request",
+            "Decline Request",
+            "Query Request"
+          ],
+          "systemActions": [
+            "Redirect",
+            "Release",
+            "Share",
+            "Sleep",
+            "Unshare"
+          ]
+        },
+        "instruction": ""
+      }
+    ]
+  }
+
+  export default sampleData;
